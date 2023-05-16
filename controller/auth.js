75d0809a3edad58d6ac11a97a7389d0198fdedfa
Daniel Mateu <danielmateu86@gmail.com>
2023-05-16 13:53:48 +0200

    Generacion JWT en Registro y Login

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
 
 
@@ -20,17 +21,21 @@ const crearUsuario = async (req, res = response) => {
             });
         }
 
-        // Encriptar contraseña
-        const salt = bcrypt.genSaltSync();
-        
+
         // Guardar usuario
         const usuario = new Usuario(req.body);
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
         usuario.password = bcrypt.hashSync(password, salt);
-        
+
         await usuario.save();
 
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
         res.json({
-            usuario
+            usuario,
+            token
         });
 
 
@@ -46,14 +51,46 @@ const crearUsuario = async (req, res = response) => {
 // login
 const login = async (req, res) => {
 
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
+        const existeEmail = await Usuario.findOne({ email });
 
-    res.json({
-        ok: true,
-        msg: 'Login',
-        email,
-        password
-    });
+        if (!existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo no existe'
+            });
+        }
+
+        // Confirmar los passwords
+        const validPassword = bcrypt.compareSync(password, existeEmail.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(existeEmail.id);
+
+        res.json({
+            ok: true,
+            msg: 'Login',
+            email,
+            password,
+            token
+
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 }
 
 // renewToken
@@ -63,8 +100,6 @@ const renewToken = async (req, res) => {
         msg: 'Renew'
     });
 }
-
-
 
 module.exports = {
     crearUsuario,
