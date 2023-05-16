a98b5cbe52c317a35df4fa0a962485e1e4fa52a2
Daniel Mateu <danielmateu86@gmail.com>
2023-05-16 11:00:55 +0200

    Schema de Mensaje + Usuario

---
+++ models/mensaje.js
@@ -0,0 +1,27 @@
+
+const { Schema, model } = require('mongoose');
+
+const MensajeSchema = Schema({
+    de: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Usuario'
+    },
+    para: {
+        type: String,
+        required: true,
+        ref: 'Usuario'
+    },
+    mensaje: {
+        type: String,
+        required: true
+    },
+    timeStamps: true
+})
+
+UsuarioSchema.method('toJSON', function () {
+    const { __v, ...object } = this.toObject();
+    return object;
+})
+
+module.exports = model('Mensaje', MensajeSchema);
---
+++ models/usuario.js
@@ -0,0 +1,31 @@
+
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    online: {
+        type: Boolean,
+        default: false
+    }
+})
+
+UsuarioSchema.method('toJSON', function(){
+    const { __v, _id, password, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+})
+
+module.exports = model('Usuario', UsuarioSchema);
