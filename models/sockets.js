7b3d30a64b991e8407988a54755fe863e3475c3c
Daniel Mateu <danielmateu86@gmail.com>
2023-05-17 14:15:59 +0200

    Inicio de autenticación de sockets

--- models/sockets.js
+++ models/sockets.js
@@ -2,7 +2,7 @@
 
 class Sockets {
 
-    constructor( io ) {
+    constructor(io) {
 
         this.io = io;
 
@@ -11,16 +11,25 @@ class Sockets {
 
     socketEvents() {
         // On connection
-        this.io.on('connection', ( socket ) => {
+        this.io.on('connection', (socket) => {
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
+            console.log('Cliente conectado');
+
+            // TODO Validar el JWT
+            // Si el token no es válido, desconectar
+
+            // Saber qué usuario está activo mediante el UID
+
+            // TODO Emitir todos los usuarios conectados
+
+            // TODO Socket join, uid
+
+            // TODO Escuchar cuando el cliente manda un mensaje -> mensaje-personal
+
+            // TODO Disconnect
+            // Marcar en la BD que el usuario se desconectó
+            // TODO Emitir todos los usuarios conectados
+
         });
     }
 
